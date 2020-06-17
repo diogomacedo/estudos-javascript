74c102c8b85aae71f4a1ddcf0c626db72101f681
Diogo Macedo <diogobr@gmail.com>
2020-06-16 23:01:25 -0300

    Adicionados arquivos com detalhes e testes sobre 'variáveis' em javascript.

---
+++ curiosidades-extras.js
@@ -0,0 +1,29 @@
+// Exemplo 1
+
+// Como foi escrito
+// const hey = 'hey'
+// const you = 'hey'
+// const heyYou = hey + ' ' + you
+
+// ['h', 'e', 'y'].forEach((letter) => console.log(letter))
+
+// Como javascript lê
+// const hey = 'hey';
+// const you = 'hey';
+// const heyYou = hey + ' ' + you['h', 'e', 'y'].forEach((letter) => console.log(letter))
+
+
+// Exemplo 2
+// (1 + 2).toString()
+
+// como foi escrito
+// const a = 1
+// const b = 2
+// const c = a + b
+// (a + b).toString()
+
+// como javascript lÊ
+// const a = 1
+// const b = 2
+// const c = a + b(a + b).toString()
+
---
+++ estudo-de-variaveis.js
@@ -0,0 +1,84 @@
+// console.log('Testes com o \'var\' ');
+
+// console.log('Antes', pi);
+// var pi = 3.141592;
+// console.log('Depois', pi);
+
+
+// pi = 3;
+// console.log('Reatribuição', pi);
+
+
+// console.log('Parte estranha:');
+// var pi=2;
+// console.log('redeclaração e reatribuição', pi);
+
+
+// console.log('Acessando variável fora do escopo, antes do escopo', x);
+// if (true) {
+//   var x = 3.141592;
+// }
+// console.log('Acessando variável fora do escopo', x);
+
+// console.log('Testes com o \'let\' ');
+
+// console.log('Antes', pi); <-- lança erro 'ReferenceError'
+// let pi = 3.141592;
+// console.log('Depois', pi);
+
+// pi = 3;
+// console.log('Reatribuição', pi);
+
+// let pi = 2; // <-- lança erro 'SyntaxError'
+// console.log('Redeclaração e reatribuição', pi);
+
+// if (true) {
+//   let x = 3.141592;
+// }
+// console.log('Acessando variável fora do escopo', x); <-- lança erro 'ReferenceError'
+
+
+// console.log('Testes com o \'const\' ');
+
+// console.log('Antes', pi); <-- lança erro 'ReferenceError'
+// const pi = 3.141592;
+// console.log('Depois', pi);
+
+// pi = 3; <-- lança erro 'TypeError'
+// console.log('Reatribuição', pi);
+
+// const pi = 2; // <-- lança erro 'SyntaxError'
+// console.log('Redeclaração e reatribuição', pi);
+
+// if (true) {
+//   let x = 3.141592;
+// }
+// console.log('Acessando variável fora do escopo', x); <-- lança erro 'ReferenceError'
+
+// console.log('Declaração de variável global');
+
+// (function(){
+//   // var pi = 3.141592;
+//   pi = 3.141592;
+// })();
+
+// console.log(pi);
+
+// console.log('Identificadores');
+
+// let name;
+// let Name;
+// let _name;
+// let $name;
+// let name123;
+// let Name123;
+// let _name123;
+// let $name123;
+
+// let 1aaaaa; <-- lança erro 'SyntaxError'
+// let @jojojo; <-- lança erro 'SyntaxError'
+
+
+
+
+
