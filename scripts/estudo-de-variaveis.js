95d5cb5bbf031f03c07ae5a3af6505e41e7f8ac2
Diogo Macedo <diogobr@gmail.com>
2026-01-05 22:39:10 -0300

    Atualizado resumo sobre variáveis.

--- scripts/estudo-de-variaveis.js
+++ scripts/estudo-de-variaveis.js
@@ -1,21 +1,3 @@
-// console.log('Testes com o \'let\' ');
-
-// console.log('Antes', pi); <-- lança erro 'ReferenceError'
-// let pi = 3.141592;
-// console.log('Depois', pi);
-
-// pi = 3;
-// console.log('Reatribuição', pi);
-
-// let pi = 2; // <-- lança erro 'SyntaxError'
-// console.log('Redeclaração e reatribuição', pi);
-
-// if (true) {
-//   let x = 3.141592;
-// }
-// console.log('Acessando variável fora do escopo', x); <-- lança erro 'ReferenceError'
-
-
 // console.log('Testes com o \'const\' ');
 
 // console.log('Antes', pi); <-- lança erro 'ReferenceError'
